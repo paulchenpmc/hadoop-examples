fd836db4b2cf8aa5978ecd548a82e4eef25ade77
Paul <paulchenpmc@gmail.com>
2019-10-30 19:04:58 -0600

    Increase memory efficiency of ex3

--- A2/a2m3.py
+++ A2/a2m3.py
@@ -14,5 +14,6 @@ for line in sys.stdin:
     all_pairs = list(itertools.permutations(items, 2))
     # Emit
     for pair in all_pairs:
-        key = '{}\t{}'.format(pair[0], pair[1])
-        print('{}\t{}'.format(key, 1))
+        key = '{}\t{}'.format(pair[0], pair[1]) # Make 2 part key
+        print('{}\t-\t1'.format(pair[0])) # Emit just the first key to count denominator, '-' is to guarantee first place in sorting order
+        print('{}\t1'.format(key)) # Emit both grocery items as 2 part key
--- A2/a2r3.py
+++ A2/a2r3.py
@@ -2,29 +2,32 @@
 #reducer.py
 
 import sys
-from collections import defaultdict
-keydict = defaultdict(int)
-prevk = None
+prevk1      = None
+prevk2      = '-' # '-' comes before all alphanumeric chars in the ascii table
+DENOMINATOR = 0
+PAIR_COUNT  = 0
 
 for kvp in sys.stdin:
     kvp = kvp.strip()
     key1, key2, _ = kvp.split('\t')
     pair = '{},{}'.format(key1, key2)
     # Track all pairs until key1 changes
-    if key1 != prevk and prevk is not None:
-        N = sum(keydict.values())
-        for key in keydict:
-            probability = keydict[key] / float(N)
-            pair_output = '{},{}'.format(key.split(',')[1], key.split(',')[0]) # Flip key order for output
-            print('{}\t{}'.format(pair_output, probability))
-        keydict.clear()
-    keydict[pair] += 1
-    prevk = key1
+    if key2 != prevk2 and prevk2 is not '-':
+        probability = PAIR_COUNT / float(DENOMINATOR)
+        pair_output = '{},{}'.format(prevk1, prevk2)
+        print('{}\t{}'.format(pair_output, probability))
+        PAIR_COUNT = 0
+    if key2 == '-' and prevk2 != '-':
+        DENOMINATOR = 0
+    if key2 == '-':
+        DENOMINATOR += 1
+    else:
+        PAIR_COUNT += 1
+    prevk1 = key1
+    prevk2 = key2
+
 
 # Emit last key group
-N = sum(keydict.values())
-for key in keydict:
-    probability = keydict[key] / float(N)
-    pair_output = '{},{}'.format(key.split(',')[1], key.split(',')[0]) # Flip key order for output
-    print('{}\t{}'.format(pair_output, probability))
-keydict.clear()
+probability = PAIR_COUNT / float(DENOMINATOR)
+pair_output = '{},{}'.format(key1, key2)
+print('{}\t{}'.format(pair_output, probability))
